a9f3a956fd0fd26abd5bb132444fcdcb7bdbf9a7
zhubaile <zyangzhuang@qq.com>
2017-07-17 19:05:26 +0800

    seven file

--- 2017-7-15/shiwu.js
+++ 2017-7-15/shiwu.js
@@ -3,7 +3,12 @@
  */
 function MyClick()
 {
-    var iNuml = 10;
-    var bFlag = true;
-
+    var aTeam = ["aaa","bbb","ccc","ddd"];
+/*    alert(aTeam[0]);
+    alert(aTeam.toString());
+    alert(aTeam.join("+").toString());
+    alert(aTeam.reverse().toString());*/
+    var sString = "abc_bca_cba";
+    var aZhu1 = sString.split("_");
+    alert(aZhu1[1]);
 }
