a4d322684c37ba5243f7aedeb2f37f6e50705f88
zhubaile <zyangzhuang@qq.com>
2017-07-21 20:06:48 +0800

    ershiyi file

---
+++ 2017-7-21/ershiyi.js
@@ -0,0 +1,68 @@
+/**
+ * Created by lenovo on 2017/7/21.
+ */
+function MyClick(){
+
+/*    var arr=[7,8,3,10,150,99,555,2];
+    var isum= 0;
+    for(var i=0;i<=arr.length-2;i++){
+        for(var j=i+1;j<=arr.length-1;j++){
+            if(arr[i]>arr[j]){
+                isum=arr[i];
+                arr[i]=arr[j];
+                arr[j]=isum;
+            }
+        }
+    }
+    alert(arr);*/
+
+
+
+
+/*    var isum=0;
+    for(var i=1;i<101;i++){
+        isum+=i;
+    }
+    alert(isum);*/
+
+
+
+/*
+    var arr=[7,8,3,10,150,99,555,2];
+    for(var i=0;i<arr.length;i++){
+        arr[i]=arr[i]+6;
+    }
+    alert(arr);*/
+
+
+
+//alert(Math.max(10,20,30,40,50,60));//获取这中间的最大值
+//alert(Math.min(10,20,30,40,50,60));//min最小值。
+//    alert(Math.round(12.56));//四舍五入
+//    alert(Math.ceil(12.1));//向上舍入，不管小数点后面都进一；
+//alert(Math.floor(12.1));//向下舍入，和上一个相反的；
+//alert(Math.random());//0-1之间的随机数，但是不包括0和1；
+//    alert(Math.floor(Math.random()*100+1));
+    //Math.random()是随机数0-1；乘以100就是0-100，但是不包括0和100，所以+1就是1-101；让后有floor，向下舍入就是1-100；
+
+
+
+
+/*var arr=[5,9,89,62,4,35,67];
+    var isum=0;
+    for(var i=0;i<arr.length-1;i++){
+        for(var j=i+1;j<arr.length;j++){
+            if(arr[i]<arr[j]){
+                isum=arr[i];
+                arr[i]=arr[j];
+                arr[j]=isum;
+            }
+        }
+    }
+alert(isum);//最小的值或者最大的值，alert（arr）就是从大到小或者 从小到大排列顺序！*/
+
+
+alert("你好我会微笑");
+    
+}
+
