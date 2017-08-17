fac08ba3675754981865c50fbb9204bb470f929d
zhubaile <zyangzhuang@qq.com>
2017-08-17 12:40:45 +0800

    shiri file

--- login/js/Login.js
+++ login/js/Login.js
@@ -1,11 +1,16 @@
 /**
  * Created by lenovo on 2017/8/9.
  */
-function login()
-{
-    var name= $.trim($("#username").val());
-    var pwd= $.trim($("#userpwd").val());
-    if(name==""||pwd==""){
-        alert("用户名或密码不正确")
-    }
-}
+$(function(){
+    var $userName=$("#userName");
+    $("#loginSubmit").on("click",function(){
+        var userNameVal=$userName.val();
+        debugger;
+        //验证输入用户名内容是否为邮箱格式
+        if(/^[A-Za-z\d]+([-_.][A-Za-z\d]+)*@([A-Za-z\d]+[-.])+[A-Za-z\d]{2,4}$/.test(userNameVal)){
+            alert("提交成功")
+        }else{
+            $userName.siblings("span").closest(".box-row").addClass("error")
+        }
+    })
+});
